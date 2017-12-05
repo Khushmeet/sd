343afc5a91b295aa4d49451452fc5ee293597dae
khushmeet <khushmeetsingh1996@gmail.com>
2017-12-06 02:29:11 +0530

    working language application in go

--- sdc/transpiler.go
+++ sdc/transpiler.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	SdParser "sd/parser"
+
 	"github.com/antlr/antlr4/runtime/Go/antlr"
-	"./parser"
 )
 
+type sdTranspilerListener struct {
+	*SdParser.BasesdListener
+}
+
+func newSdTranspilerListener() *sdTranspilerListener {
+	return new(sdTranspilerListener)
+}
+
+func (sd *sdTranspilerListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+	fmt.Println(ctx.GetText())
+}
+
 func main() {
-	fmt.Println("SD")
+	input, _ := antlr.NewFileStream(os.Args[1])
+	lexer := SdParser.NewsdLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	parser := SdParser.NewsdParser(stream)
+	parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	parser.BuildParseTrees = true
+	tree := parser.Expr()
+	antlr.ParseTreeWalkerDefault.Walk(newSdTranspilerListener(), tree)
 }

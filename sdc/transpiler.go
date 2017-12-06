6533677ea5c357a7590d8e87f7c8be704f96842a
khushmeet <khushmeetsingh1996@gmail.com>
2017-12-06 14:38:58 +0530

    testing go parser

--- sdc/transpiler.go
+++ sdc/transpiler.go
@@ -2,31 +2,23 @@ package main
 
 import (
 	"fmt"
-	"os"
 	SdParser "sd/parser"
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
-type sdTranspilerListener struct {
+type SdTranspilerListener struct {
 	*SdParser.BasesdListener
 }
 
-func newSdTranspilerListener() *sdTranspilerListener {
-	return new(sdTranspilerListener)
+/*
+NewSdTranspilerListener creates new sdTranspilerListener object
+*/
+func NewSdTranspilerListener() *SdTranspilerListener {
+	return new(SdTranspilerListener)
 }
 
-func (sd *sdTranspilerListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+// EnterEveryRule gets called when tree walker enter a node in parse tree
+func (sd *SdTranspilerListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
 	fmt.Println(ctx.GetText())
-}
-
-func main() {
-	input, _ := antlr.NewFileStream(os.Args[1])
-	lexer := SdParser.NewsdLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
-	parser := SdParser.NewsdParser(stream)
-	parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	parser.BuildParseTrees = true
-	tree := parser.Expr()
-	antlr.ParseTreeWalkerDefault.Walk(newSdTranspilerListener(), tree)
 }
